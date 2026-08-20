4f1ccbd26a072ee5392932a36d7a874eba26211e
fmesta35 <fmesta35@gmail.com>
2026-08-21 01:06:00 +0300

    config.php

--- yoncu-api/config.php
+++ yoncu-api/config.php
@@ -4,7 +4,7 @@
 define('GV_DB_HOST', 'localhost');
 define('GV_DB_NAME', 'masaoyun_db');
 define('GV_DB_USER', 'masaoyun_kurucu');
-define('GV_DB_PASS', 'BURAYA_MYSQL_SIFRENI_YAZ');   // ← oPanel'de belirlediğin şifre
+define('GV_DB_PASS', 'kurucu123');   // ← oPanel'de belirlediğin şifre
 
 define('GV_SERVER_KEY', 'xK9$mQ2vLp#7nB4wR8zT3yU6eF1aS5dG0hJ');
 
@@ -16,4 +16,4 @@ define('GV_MAIL_FROM_ADDR', 'info@masaoyunlari.com.tr');
 define('GV_SMTP_HOST', 'mail.masaoyunlari.com.tr');
 define('GV_SMTP_PORT', 465);
 define('GV_SMTP_USER', 'info@masaoyunlari.com.tr');
-define('GV_SMTP_PASS', '3198HYgh@');
+define('GV_SMTP_PASS', 'F1905gs.!');
